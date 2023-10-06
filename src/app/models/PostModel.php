a5b7f9cd389ae58d11b513493d775e5908f69a85
Genvictus <johannstarkmk42@gmail.com>
2023-10-06 16:46:16 +0700

    add: model for posts

---
+++ src/app/models/PostModel.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once APP_ROOT_PATH . '/app/baseclasses/BaseModel.php';
+
+class PostModel extends BaseModel
+{
+    public $post_id;
+    public $owner_id;
+    public $body;
+    public $created_at;
+    // public $refer_type;
+    // public $refer_post;
+    // public $refer_post_owner;
+    public $tags;
+
+    private function __construct()
+    {
+        $this->_attributes = ['post_id', 'owner_id', 'body', 'created_at', 'tags'];
+        $this->_primary_key = ['post_id', 'owner_id'];
+    }
+}
