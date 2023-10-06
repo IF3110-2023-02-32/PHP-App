2e30220f1864c71e0f1c8f70379021b87c229a0e
Genvictus <johannstarkmk42@gmail.com>
2023-10-06 16:45:09 +0700

    feat: implements constructFromArray & toArray

--- src/app/baseclasses/BaseModel.php
+++ src/app/baseclasses/BaseModel.php
@@ -2,20 +2,37 @@
 
 abstract class BaseModel {
   public $_primary_key = '';
+  protected $_attributes;
 
   public function __construct() {
     return $this;
   }
 
-  public function set($attr, $value) {
-    $this->$attr = $value;
+  public function set($attribute, $value) {
+    $this->$attribute = $value;
     return $this;
   }
 
-  public function get($attr) {
-    return $this->$attr;
+  public function get($attribute) {
+    return $this->$attribute;
   }
   
-  abstract public function constructFromArray($array);
-  abstract public function toResponse();
+  public function constructFromArray($array)
+  {
+    foreach ($this->_attributes as $attribute) {
+      $this->$attribute = $array[$attribute];
+    }
+
+    return $this;
+  }
+
+  public function toArray()
+  {
+    $array = [];
+    foreach ($this->_attributes as $attribute) {
+      $array[$attribute] = $this->$attribute;
+    }
+
+    return $array;
+  }
 }
