51c935347a59c68db8048960c354aa2c0a999de4
Genvictus <johannstarkmk42@gmail.com>
2023-10-06 18:30:44 +0700

    update: new router & index.php

--- src/index.php
+++ src/index.php
@@ -3,9 +3,11 @@
 define('APP_ROOT_PATH', __DIR__);
 
 require_once "app/config/config.php";
-require_once APP_ROOT_PATH . "/app/router/PageRouter.php";
+require_once APP_ROOT_PATH . "/app/router/Router.php";
 require_once APP_ROOT_PATH . "/routes.php";
 
-$router = new PageRouter($routes);
-$router->setErrorRoute("/public/view/404.php");
-$router->routing($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$router = new Router();
+
+// $router->addHandler("/example", BaseController::getInstance(), [BaseMiddleware::getInstance()]);
+
+$router->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
