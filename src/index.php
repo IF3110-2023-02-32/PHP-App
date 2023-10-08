135ca5f929815b7c44b2eb012825650b630ad078
Genvictus <johannstarkmk42@gmail.com>
2023-10-08 17:37:38 +0700

    feat: connect create post page

--- src/index.php
+++ src/index.php
@@ -8,19 +8,24 @@ define('MIDDLEWARE_PATH', __DIR__ . '/app/middlewares');
 require_once "app/config/config.php";
 require_once APP_ROOT_PATH . "/app/router/Router.php";
 require_once CONTROLLER_PATH . "/User/LoginController.php";
-require_once CONTROLLER_PATH . "/User/PageLogin.php";
 require_once CONTROLLER_PATH . "/User/RegisterController.php";
 require_once CONTROLLER_PATH . "/Admin/AdminController.php";
 require_once CONTROLLER_PATH . "/Admin/BanController.php";
 require_once CONTROLLER_PATH . "/Admin/UnbanController.php";
+
+require_once CONTROLLER_PATH . "/Page/LoginPage.php";
+require_once CONTROLLER_PATH . "/Page/ComposePage.php";
+
 $router = new Router();
 
 // $router->addHandler("/example", BaseController::getInstance(), [BaseMiddleware::getInstance()]);
 $router->addHandler("/api/login", LoginController::getInstance(), []);
-$router->addHandler("/login", LoginPage::getInstance(), []);
 $router->addHandler("/api/register", RegisterController::getInstance(), []);
 $router->addHandler("/api/admin", AdminController::getInstance(), []);
 $router->addHandler("/api/ban", BanController::getInstance(), []);
 $router->addHandler("/api/unban", UnbanController::getInstance(), []);
+
+$router->addHandler("/login", LoginPage::getInstance(), []);
+$router->addHandler("/compose/kicau", ComposePage::getInstance(), []);
 
 $router->run($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
