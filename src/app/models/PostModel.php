6505ad99f0a3f1dda9725c81987084850fd7cd9e
Genvictus <johannstarkmk42@gmail.com>
2023-10-08 15:08:47 +0700

    update: post model

--- src/app/models/PostModel.php
+++ src/app/models/PostModel.php
@@ -2,20 +2,21 @@
 
 require_once APP_ROOT_PATH . '/app/baseclasses/BaseModel.php';
 
-class PostModel
+class PostModel extends BaseModel
 {
     public $post_id;
     public $owner_id;
     public $body;
     public $created_at;
-    // public $refer_type;
-    // public $refer_post;
-    // public $refer_post_owner;
-    public $tags;
+
+    public $refer_type = null;
+    public $refer_post = null;
+    public $refer_post_owner = null;
+
+    public $tags = [];
 
     private function __construct()
     {
-        $this->_attributes = ['post_id', 'owner_id', 'body', 'created_at', 'tags'];
         $this->_primary_key = ['post_id', 'owner_id'];
     }
 }
